fdc9ba98910322a7cc27dfc7899d1d7d9d8a08bf
¨marisaroude¨ <marisaroude1@gmail.com>
2024-06-05 21:56:36 -0300

    swagger config

---
+++ swagger.js
@@ -0,0 +1,21 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation SuperHero',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./src/routes/*.js'], // Rutas de tus archivos de rutas
+};
+
+const specs = swaggerJSDoc(options);
+
+module.exports = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+};
+
+
