29aaa2cc4f8420a604878cec73b5bebd8fb10d08
¨marisaroude¨ <marisaroude1@gmail.com>
2024-06-26 23:07:58 -0300

    add data when db is empty

--- src/api/app.js
+++ src/api/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongodb = require('../db/mongo');
 const router = require('../routes/index');
 const swaggerConfig = require('../../swagger');
+const { addData } = require('../test/addData');
 
 const app = express();
 
@@ -9,13 +10,44 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+  });
+
 swaggerConfig(app);
 
+async function isDatabaseEmpty() {
+  try {
+    
+      const count = await mongodb.db.collection('heros').countDocuments();
+      console.log('Número de documentos en la colección heroes:', count);
+      return count === 0;
+  } catch (error) {
+      console.error('Error al verificar si la colección heroes está vacía:', error);
+      throw error;
+  }
+}
+
 // Conexión a MongoDB
-mongodb.once('open', () => {
-    console.log('Conexión a MongoDB establecida');
-    // Ahora puedes hacer cualquier cosa que necesites con la conexión a MongoDB
-    // addData()
+mongodb.once('open', async () => {
+    
+  try {
+    const empty = await isDatabaseEmpty();
+    if (empty) {
+        console.log('La base de datos está vacía, añadiendo datos...');
+        addData();
+    } else {
+        console.log('La base de datos no está vacía.');
+    }
+    } catch (error) {
+      console.error('Error al verificar o agregar datos:', error);
+    }
 });
 
 app.use("/superheroes", router);
